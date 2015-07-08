63cd793c9607d80ff2ac3b729d60badb7313aae9
j1r1k <jirik@atlas.marsicek.net>
2015-07-08 14:54:14 +0200

    Initial rewrite

--- bash-powerline.sh
+++ bash-powerline.sh
@@ -1,110 +1,124 @@
 #!/usr/bin/env bash
 
+#set -x
 __powerline() {
+  # Unicode symbols
+  readonly PS_SYMBOL_USER='$'
+  readonly PS_SYMBOL_ROOT='#'
 
-    # Unicode symbols
-    readonly PS_SYMBOL_DARWIN=''
-    readonly PS_SYMBOL_LINUX='$'
-    readonly PS_SYMBOL_OTHER='%'
-    readonly GIT_BRANCH_SYMBOL='⑂ '
-    readonly GIT_BRANCH_CHANGED_SYMBOL='+'
-    readonly GIT_NEED_PUSH_SYMBOL='⇡'
-    readonly GIT_NEED_PULL_SYMBOL='⇣'
+  readonly PS_DELIM=''
+  readonly PS_DELIM_THIN=''
 
-    # Solarized colorscheme
-    readonly FG_BASE03="\[$(tput setaf 8)\]"
-    readonly FG_BASE02="\[$(tput setaf 0)\]"
-    readonly FG_BASE01="\[$(tput setaf 10)\]"
-    readonly FG_BASE00="\[$(tput setaf 11)\]"
-    readonly FG_BASE0="\[$(tput setaf 12)\]"
-    readonly FG_BASE1="\[$(tput setaf 14)\]"
-    readonly FG_BASE2="\[$(tput setaf 7)\]"
-    readonly FG_BASE3="\[$(tput setaf 15)\]"
+  readonly GIT_BRANCH_SYMBOL=' '
+  readonly GIT_BRANCH_CHANGED_SYMBOL='±'
+  readonly GIT_NEED_PUSH_SYMBOL='⇡'
+  readonly GIT_NEED_PULL_SYMBOL='⇣'
+  
+  # Colorscheme
+ 
+  readonly FG_00="\[$(tput setaf 0)\]"  # black
+  readonly FG_08="\[$(tput setaf 8)\]"  # gray
+  readonly FG_01="\[$(tput setaf 1)\]"  # red
+  readonly FG_09="\[$(tput setaf 9)\]"  # light red
+  readonly FG_02="\[$(tput setaf 2)\]"  # green
+  readonly FG_10="\[$(tput setaf 10)\]" # light green
+  readonly FG_03="\[$(tput setaf 3)\]"  # orange
+  readonly FG_11="\[$(tput setaf 11)\]" # yellow
+  readonly FG_04="\[$(tput setaf 4)\]"  # blue
+  readonly FG_12="\[$(tput setaf 12)\]" # light blue
+  readonly FG_05="\[$(tput setaf 5)\]"  # purple
+  readonly FG_13="\[$(tput setaf 13)\]" # light purple
+  readonly FG_06="\[$(tput setaf 6)\]"  # aqua
+  readonly FG_14="\[$(tput setaf 14)\]" # cyan
+  readonly FG_07="\[$(tput setaf 7)\]"  # selection
+  readonly FG_15="\[$(tput setaf 15)\]" # current line
 
-    readonly BG_BASE03="\[$(tput setab 8)\]"
-    readonly BG_BASE02="\[$(tput setab 0)\]"
-    readonly BG_BASE01="\[$(tput setab 10)\]"
-    readonly BG_BASE00="\[$(tput setab 11)\]"
-    readonly BG_BASE0="\[$(tput setab 12)\]"
-    readonly BG_BASE1="\[$(tput setab 14)\]"
-    readonly BG_BASE2="\[$(tput setab 7)\]"
-    readonly BG_BASE3="\[$(tput setab 15)\]"
+  readonly BG_00="\[$(tput setab 0)\]"  # black
+  readonly BG_08="\[$(tput setab 8)\]"  # gray
+  readonly BG_01="\[$(tput setab 1)\]"  # red
+  readonly BG_09="\[$(tput setab 9)\]"  # light red
+  readonly BG_02="\[$(tput setab 2)\]"  # green
+  readonly BG_10="\[$(tput setab 10)\]" # light green
+  readonly BG_03="\[$(tput setab 3)\]"  # orange
+  readonly BG_11="\[$(tput setab 11)\]" # yellow
+  readonly BG_04="\[$(tput setab 4)\]"  # blue
+  readonly BG_12="\[$(tput setab 12)\]" # light blue
+  readonly BG_05="\[$(tput setab 5)\]"  # purple
+  readonly BG_13="\[$(tput setab 13)\]" # light purple
+  readonly BG_06="\[$(tput setab 6)\]"  # aqua
+  readonly BG_14="\[$(tput setab 14)\]" # cyan
+  readonly BG_07="\[$(tput setab 7)\]"  # selection
+  readonly BG_15="\[$(tput setab 15)\]" # current line
 
-    readonly FG_YELLOW="\[$(tput setaf 3)\]"
-    readonly FG_ORANGE="\[$(tput setaf 9)\]"
-    readonly FG_RED="\[$(tput setaf 1)\]"
-    readonly FG_MAGENTA="\[$(tput setaf 5)\]"
-    readonly FG_VIOLET="\[$(tput setaf 13)\]"
-    readonly FG_BLUE="\[$(tput setaf 4)\]"
-    readonly FG_CYAN="\[$(tput setaf 6)\]"
-    readonly FG_GREEN="\[$(tput setaf 2)\]"
+  readonly DIM="\[$(tput dim)\]"
+  readonly REVERSE="\[$(tput rev)\]"
+  readonly RESET="\[$(tput sgr0)\]"
+  readonly BOLD="\[$(tput bold)\]"
+  
+  if [[ ${EUID} == 0 ]]; then
+    readonly PS_SYMBOL=${PS_SYMBOL_ROOT}
+  else
+    readonly PS_SYMBOL=${PS_SYMBOL_USER}
+  fi
+  
+  __git_info() {
+    [ -x "$(which git)" ] || return    # git not found
+  
+    # get current branch name or short SHA1 hash for detached head
+    local branch="$(git symbolic-ref --short HEAD 2>/dev/null || git describe --tags --always 2>/dev/null)"
+    [ -n "$branch" ] || return  # git branch not found
+  
+    local marks
+  
+    # branch is modified?
+    [ -n "$(git status --porcelain)" ] && marks+=" $GIT_BRANCH_CHANGED_SYMBOL"
+  
+    # how many commits local branch is ahead/behind of remote?
+    local stat="$(git status --porcelain --branch | grep '^##' | grep -o '\[.\+\]$')"
+    local aheadN="$(echo $stat | grep -o 'ahead \d\+' | grep -o '\d\+')"
+    local behindN="$(echo $stat | grep -o 'behind \d\+' | grep -o '\d\+')"
+    [ -n "$aheadN" ] && marks+=" $GIT_NEED_PUSH_SYMBOL$aheadN"
+    [ -n "$behindN" ] && marks+=" $GIT_NEED_PULL_SYMBOL$behindN"
+  
+    # print the git branch segment without a trailing newline
+    printf "$GIT_BRANCH_SYMBOL$branch$marks"
+  }
+  
+  ps1() {
+    # Check the exit code of the previous command and display different
+    # colors in the prompt accordingly.
+    if [ $? -eq 0 ] 
+    then
+      local EXIT=""
+    else
+      local EXIT="${BOLD}${FG_01}"
+    fi
 
-    readonly BG_YELLOW="\[$(tput setab 3)\]"
-    readonly BG_ORANGE="\[$(tput setab 9)\]"
-    readonly BG_RED="\[$(tput setab 1)\]"
-    readonly BG_MAGENTA="\[$(tput setab 5)\]"
-    readonly BG_VIOLET="\[$(tput setab 13)\]"
-    readonly BG_BLUE="\[$(tput setab 4)\]"
-    readonly BG_CYAN="\[$(tput setab 6)\]"
-    readonly BG_GREEN="\[$(tput setab 2)\]"
+    PS1=""
 
-    readonly DIM="\[$(tput dim)\]"
-    readonly REVERSE="\[$(tput rev)\]"
-    readonly RESET="\[$(tput sgr0)\]"
-    readonly BOLD="\[$(tput bold)\]"
+    if [[ ${EUID} == 0 ]]
+    then
+	PS1+="${BG_09}${FG_15}${BOLD}\h${FG_09}${BG_15}${PS_DELIM}${RESET}"
+    else
+	PS1+="${BG_10}${FG_00}${BOLD}\h${FG_10}${BG_15}${PS_DELIM}${RESET}"
+    fi
 
-    # what OS?
-    case "$(uname)" in
-        Darwin)
-            readonly PS_SYMBOL=$PS_SYMBOL_DARWIN
-            ;;
-        Linux)
-            readonly PS_SYMBOL=$PS_SYMBOL_LINUX
-            ;;
-        *)
-            readonly PS_SYMBOL=$PS_SYMBOL_OTHER
-    esac
+    PS1+="${BG_15}${FG_12} \w ${RESET}"
 
-    __git_info() { 
-        [ -x "$(which git)" ] || return    # git not found
+    GIT_INFO=$(__git_info)
 
-        local git_eng="env LANG=C git"   # force git output in English to make our work easier
-        # get current branch name or short SHA1 hash for detached head
-        local branch="$($git_eng symbolic-ref --short HEAD 2>/dev/null || $git_eng describe --tags --always 2>/dev/null)"
-        [ -n "$branch" ] || return  # git branch not found
+    if [[ -n ${GIT_INFO} ]]
+    then
+      PS1+="${FG_15}${BG_12}${PS_DELIM}${FG_15}${BG_12} ${GIT_INFO} ${RESET}${FG_12}${PS_DELIM}${RESET}"
+    else
+      PS1+="${RESET}${FG_15}${PS_DELIM}${RESET}"
+    fi
 
-        local marks
+    PS1+=" ${EXIT}${PS_SYMBOL}${RESET} "
+  }
 
-        # branch is modified?
-        [ -n "$($git_eng status --porcelain)" ] && marks+=" $GIT_BRANCH_CHANGED_SYMBOL"
-
-        # how many commits local branch is ahead/behind of remote?
-        local stat="$($git_eng status --porcelain --branch | grep '^##' | grep -o '\[.\+\]$')"
-        local aheadN="$(echo $stat | grep -o 'ahead [[:digit:]]\+' | grep -o '[[:digit:]]\+')"
-        local behindN="$(echo $stat | grep -o 'behind [[:digit:]]\+' | grep -o '[[:digit:]]\+')"
-        [ -n "$aheadN" ] && marks+=" $GIT_NEED_PUSH_SYMBOL$aheadN"
-        [ -n "$behindN" ] && marks+=" $GIT_NEED_PULL_SYMBOL$behindN"
-
-        # print the git branch segment without a trailing newline
-        printf " $GIT_BRANCH_SYMBOL$branch$marks "
-    }
-
-    ps1() {
-        # Check the exit code of the previous command and display different
-        # colors in the prompt accordingly. 
-        if [ $? -eq 0 ]; then
-            local BG_EXIT="$BG_GREEN"
-        else
-            local BG_EXIT="$BG_RED"
-        fi
-
-        PS1="$BG_BASE1$FG_BASE3 \w $RESET"
-        PS1+="$BG_BLUE$FG_BASE3$(__git_info)$RESET"
-        PS1+="$BG_EXIT$FG_BASE3 $PS_SYMBOL $RESET "
-    }
-
-    PROMPT_COMMAND=ps1
+  PROMPT_COMMAND=ps1
 }
-
 __powerline
 unset __powerline
+
