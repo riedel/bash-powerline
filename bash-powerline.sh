ca50a96ba9d49399348b298fef49fc9e1ab1c84b
j1r1k <jiri.marsicek@gmail.com>
2015-09-15 23:07:34 +0200

    Combines script into one file, Fixes everything

--- bash-powerline.sh
+++ bash-powerline.sh
@@ -1,17 +1,64 @@
 #!/bin/bash
 
-GIT_CHECK="/usr/local/bin/git-check"
-GIT_INFO="/usr/local/bin/git-info"
-PWDS="/usr/local/bin/pwds"
+PS_DELIM=''
+ 
+function pwds () {
 
-PWD_LENGTH=25
+  LENGTH=25
+  readonly PWDS_SYMBOL='…'
 
-if [[ -n ${PS_POWERLINE} ]]
-then
-  PS_DELIM=''
-else
-  PS_DELIM=''
-fi
+  PRE=""
+  NAME="${1}"
+  
+  if [[ "${NAME}" != "${NAME#${HOME}/}" || -z "${NAME#${HOME}}" ]]
+  then
+    PRE+='~' NAME="${NAME#${HOME}}" LENGTH=$[LENGTH-1]
+  fi
+  if ((${#NAME}>${LENGTH}))
+  then 
+    NAME="/${PWDS_SYMBOL}${NAME:$[${#NAME}-LENGTH+1]}"
+  fi
+  
+  echo "${PRE}${NAME}" 
+
+  unset LENGTH PRE NAME
+}
+
+function git-check () {
+
+  [ -x "$(which git)" ] || exit
+  
+  # get current branch name or short SHA1 hash for detached head
+  BRANCH="$(git symbolic-ref --short HEAD 2>/dev/null || git describe --tags --always 2>/dev/null)"
+  [ -n "${BRANCH}" ] || exit
+}
+
+function git-info () {
+
+  readonly GIT_BRANCH_SYMBOL=' '
+  readonly GIT_BRANCH_CHANGED_SYMBOL='±'
+  readonly GIT_NEED_PUSH_SYMBOL='⇡'
+  readonly GIT_NEED_PULL_SYMBOL='⇣'
+
+  git-check
+ 
+  MARKS=""
+  
+  # branch is modified?
+  [ -n "$(git status --porcelain)" ] && MARKS+=" ${GIT_BRANCH_CHANGED_SYMBOL}"
+  
+  # how many commits local branch is ahead/behind of remote?
+  STAT="$(git status --porcelain --branch | grep '^##' | grep -o '\[.\+\]$')"
+  AHEADN="$(git status --porcelain --branch | sed -n 's~^\#\#.*ahead \([0-9]*\).*~\1~p')"
+  BEHINDN="$(git status --porcelain --branch | sed -n 's~^\#\#.*behind \([0-9]*\).*~\1~p')"
+  [ -n "${AHEADN}" ] && MARKS+=" ${GIT_NEED_PUSH_SYMBOL}${AHEADN}"
+  [ -n "${BEHINDN}" ] && MARKS+=" ${GIT_NEED_PULL_SYMBOL}${BEHINDN}"
+  
+  # print the git branch segment without a trailing newline
+  printf "${GIT_BRANCH_SYMBOL}${BRANCH}${MARKS}"
+
+  unset MARKS STAT AHEADN BEHINDN
+}
 
 FG_LRED='\[\e[91m\]'
 FG_GRN='\[\e[32m\]'
@@ -36,7 +83,7 @@ else
   PS1+="${BG_GRN}${FG_WHT}\h${BG_WHT}${FG_GRN}${PS_DELIM}"
 fi
 
-PS1+="\$(RET=\$?; echo -n \"${BG_WHT}${FG_LBLU} \$(${PWDS} \"\${PWD}\" ${PWD_LENGTH}) ${RESET}\" ; if \$(/usr/local/bin/git-check); then echo -n \"${FG_WHT}${BG_LBLU}${PS_DELIM}${FG_WHT}${BG_LBLU} \$(/usr/local/bin/git-info) ${RESET}${FG_LBLU}${PS_DELIM}${RESET} \"; else echo -n \"${RESET}${FG_WHT}${PS_DELIM}${RESET} \"; fi; if [[ \${RET} == 0 ]]; then echo -n \"${FG_LBLU}\";else echo -n \"${FG_LRED}\"; fi)"
+PS1+="\$(RET=\$?; echo -n \"${BG_WHT}${FG_LBLU} \$(pwds \"\${PWD}\") ${RESET}\" ; if \$(git-check); then echo -n \"${FG_WHT}${BG_LBLU}${PS_DELIM}${FG_WHT}${BG_LBLU} \$(git-info) ${RESET}${FG_LBLU}${PS_DELIM}${RESET} \"; else echo -n \"${RESET}${FG_WHT}${PS_DELIM}${RESET} \"; fi; if [[ \${RET} == 0 ]]; then echo -n \"${FG_LBLU}\";else echo -n \"${FG_LRED}\"; fi)"
 
 if [[ ${EUID} == 0 ]]
 then
@@ -47,7 +94,7 @@ fi
 
 PS1+="${RESET} "
 
-unset GIT_CHECK GIT_INFO PS_DELIM 
+unset PS_DELIM 
 unset FG_LRED FG_GRN FG_LBLU FG_WHT FG_BWHT
 unset BG_LRED BG_GRN BG_LBLU BG_WHT
 unset RESET 
--- git-check.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-[ -x "$(which git)" ] || exit
-
-# get current branch name or short SHA1 hash for detached head
-BRANCH="$(git symbolic-ref --short HEAD 2>/dev/null || git describe --tags --always 2>/dev/null)"
-[ -n "${BRANCH}" ] || exit 
