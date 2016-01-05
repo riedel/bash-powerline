f20f8c82b022c36b9fc16c571426eb66a63bc14b
U-TECO\riedel <riedel@teco.edu>
2016-01-05 18:16:29 +0100

    moreconsisten handling of colors

--- bash-powerline.sh
+++ bash-powerline.sh
@@ -31,58 +31,63 @@ __powerline() {
         # use standard fonts
         readonly PROMPT_DIVIDER=' '
         readonly PWD_DIVIDER='/'
-        readonly GIT_BRANCH_SYMBOL=' ⑂ '
+        readonly GIT_BRANCH_SYMBOL='⑂'
 
     else # use powerline-fonts
         readonly PROMPT_DIVIDER=''
         readonly PWD_DIVIDER='/'
-        readonly GIT_BRANCH_SYMBOL='  '
+        readonly GIT_BRANCH_SYMBOL=''
     fi
 
 
-
-
     # Solarized colorscheme
-    readonly FG_BASE03="\[$(tput setaf 8)\]"
-    readonly FG_BASE02="\[$(tput setaf 0)\]"
-    readonly FG_BASE01="\[$(tput setaf 7)\]"
-    readonly FG_BASE00="\[$(tput setaf 10)\]"
-    readonly FG_BASE0="\[$(tput setaf 12)\]"
-    readonly FG_BASE1="\[$(tput setaf 14)\]"
-    readonly FG_BASE2="\[$(tput setaf 7)\]"
-    readonly FG_BASE3="\[$(tput setaf 15)\]"
+    readonly FG_BASE03="$(tput setaf 8)"
+    readonly BG_BASE03="$(tput setab 8)"
 
-    readonly BG_BASE03="\[$(tput setab 8)\]"
-    readonly BG_BASE02="\[$(tput setab 0)\]"
-    readonly BG_BASE01="\[$(tput setab 7)\]"
-    readonly BG_BASE00="\[$(tput setab 10)\]"
-    readonly BG_BASE0="\[$(tput setab 12)\]"
-    readonly BG_BASE1="\[$(tput setab 14)\]"
-    readonly BG_BASE2="\[$(tput setab 7)\]"
-    readonly BG_BASE3="\[$(tput setab 15)\]"
+    readonly FG_BASE02="$(tput setaf 0)"
+    readonly BG_BASE02="$(tput setab 0)"
 
-    readonly FG_YELLOW="\[$(tput setaf 3)\]"
-    readonly FG_ORANGE="\[$(tput setaf 9)\]"
-    readonly FG_RED="\[$(tput setaf 1)\]"
-    readonly FG_MAGENTA="\[$(tput setaf 5)\]"
-    readonly FG_VIOLET="\[$(tput setaf 13)\]"
-    readonly FG_BLUE="\[$(tput setaf 4)\]"
-    readonly FG_CYAN="\[$(tput setaf 6)\]"
-    readonly FG_GREEN="\[$(tput setaf 2)\]"
+    readonly FG_BASE01="$(tput setaf 7)"
+    readonly BG_BASE01="$(tput setab 7)"
 
-    readonly BG_YELLOW="\[$(tput setab 3)\]"
-    readonly BG_ORANGE="\[$(tput setab 9)\]"
-    readonly BG_RED="\[$(tput setab 1)\]"
-    readonly BG_MAGENTA="\[$(tput setab 5)\]"
-    readonly BG_VIOLET="\[$(tput setab 13)\]"
-    readonly BG_BLUE="\[$(tput setab 4)\]"
-    readonly BG_CYAN="\[$(tput setab 6)\]"
-    readonly BG_GREEN="\[$(tput setab 2)\]"
+    readonly FG_BASE00="$(tput setaf 9)"
+    readonly BG_BASE00="$(tput setab 9)"
 
-    readonly DIM="\[$(tput dim)\]"
-    readonly REVERSE="\[$(tput rev)\]"
-    readonly RESET="\[$(tput sgr0)\]"
-    readonly BOLD="\[$(tput bold)\]"
+    readonly BG_BASE0="$(tput setab 3)"
+    readonly FG_BASE0="$(tput setaf 3)"
+
+    readonly FG_BASE1="$(tput setaf 4)"
+    readonly BG_BASE1="$(tput setab 4)"
+
+    readonly FG_BASE2="$(tput setaf 7)"
+    readonly BG_BASE2="$(tput setab 7)"
+    
+    readonly FG_BASE3="$(tput setaf 5)"
+    readonly BG_BASE3="$(tput setab 5)"
+
+
+    readonly FG_YELLOW="$(tput setaf 3)"
+    readonly FG_ORANGE="$(tput setaf 9)"
+    readonly FG_RED="$(tput setaf 1)"
+    readonly FG_MAGENTA="$(tput setaf 5)"
+    readonly FG_VIOLET="$(tput setaf 13)"
+    readonly FG_BLUE="$(tput setaf 4)"
+    readonly FG_CYAN="$(tput setaf 6)"
+    readonly FG_GREEN="$(tput setaf 2)"
+
+    readonly BG_YELLOW="$(tput setab 3)"
+    readonly BG_ORANGE="$(tput setab 9)"
+    readonly BG_RED="$(tput setab 1)"
+    readonly BG_MAGENTA="$(tput setab 5)"
+    readonly BG_VIOLET="$(tput setab 13)"
+    readonly BG_BLUE="$(tput setab 4)"
+    readonly BG_CYAN="$(tput setab 6)"
+    readonly BG_GREEN="$(tput setab 2)"
+
+    readonly DIM="$(tput dim)"
+    readonly REVERSE="$(tput rev)"
+    readonly RESET="$(tput sgr0)"
+    readonly BOLD="$(tput bold)"
 
     # what OS?
     case "$(uname)" in
@@ -99,13 +104,15 @@ __powerline() {
 
     if (hash git &> /dev/null); then
     __git_info() {
-	    git 2>/dev/null status --porcelain --branch |  sed -n -e '
-	    s/##\(.*\)\.\.\.[^\[]*/'"$PROMPT_DIVIDER$GIT_BRANCH_SYMBOL"'\1 /;
+	    git 2>/dev/null status --porcelain --branch |\
+	   sed -n -e '
+	    s/##\s\+\(.*\)\.\.\.[^\[]*/'" $GIT_BRANCH_SYMBOL"'\1/;
 	    s/ *behind \([0-9]*\)/⇣\1/;
 	    s/ *ahead \([0-9]*\)/⇡\1/;1{h};
 	    s/^ *[UDCMA].*$/+/;
-	    /^+/{H;x;s/[\n ]//g;p;q};
-	    /^\?/{x;s/[\n ]//;p;q}'
+	    /^+/{H;x;s/\n//g;p;q};
+	    /^\?/{x;p;q}
+	    '
     }
     else 
     __git_info() {
@@ -113,21 +120,6 @@ __powerline() {
     }
     fi
 
-
-    __virtualenv() {
-        # Copied from Python virtualenv's activate.sh script.
-        # https://github.com/pypa/virtualenv/blob/a9b4e673559a5beb24bac1a8fb81446dd84ec6ed/virtualenv_embedded/activate.sh#L62
-        # License: MIT
-        if [ "x$VIRTUAL_ENV" != "x" ]; then
-            if [ "`basename \"$VIRTUAL_ENV\"`" == "__" ]; then
-                # special case for Aspen magic directories
-                # see http://www.zetadev.com/software/aspen/
-                printf "[`basename \`dirname \"$VIRTUAL_ENV\"\``]"
-            else
-                printf "(`basename \"$VIRTUAL_ENV\"`)"
-            fi
-        fi
-    }
 
 
     __getpwd() {
@@ -138,42 +130,50 @@ __powerline() {
 
     ps1() {
     if [ $? -ne 0 ]; then
-      BG_EXIT="${BG_RED#\\[}"
-      FG_EXIT="${FG_RED#\\[}"
+      BG_EXIT="$BG_RED"
+      FG_EXIT="$FG_RED"
     else
-      BG_EXIT="${BG_GREEN#\\[}"
-      FG_EXIT="${FG_GREEN#\\[}"
+      BG_EXIT="$BG_GREEN"
+      FG_EXIT="$FG_GREEN"
     fi
-    BG_EXIT="${BG_EXIT%\\]}"
-    FG_EXIT="${FG_EXIT%\\]}"
   }
 
   PS1=""
 
   # username
   if [[ $EUID == 0 ]]; then
-	  PS1+="$BG_RED$FG_BASE3 \u $RESET"
-	  PS1+="$BG_BASE0$FG_RED$PROMPT_DIVIDER$RESET"
+	  PS1+="\[\e]0;[root]\w\a\]"
+
+	  PS1+="\[$BG_RED$FG_BASE3\] \u \[$RESET\]"
+	  PS1+="\[$RESET$BG_BASE0$FG_RED\]$PROMPT_DIVIDER"
   else
 	  if [[ $OSTYPE == "cygwin" ]] && net session &> /dev/null; then
-		  PS1+="$BG_RED$FG_BASE3 \u $RESET"
-		  PS1+="$BG_BASE0$FG_RED$PROMPT_DIVIDER$RESET"
+		  PS1+="\[\e]0;[root]\w\a\]"
+
+		  PS1+="\[$BG_RED$FG_BASE00\] \u \[$RESET\]"
+		  PS1+="\[$RESET$FG_RED$BG_BASE1\]$PROMPT_DIVIDER"
 	  else
-		  PS1+="$BG_BLUE$FG_BASE3 \u $RESET"
-		  PS1+="$BG_BASE00$FG_BLUE$PROMPT_DIVIDER$RESET"
+		  PS1+="\[\e]0;\w\a\]"
+
+		  PS1+="\[$BG_BASE0$FG_BASE00\] \u \[$RESET\]"
+		  PS1+="\[$RESET$FG_BASE0$BG_BASE1\]$PROMPT_DIVIDER"
 	  fi
   fi
 
-  PS1+="$BG_BASE0$FG_BASE3$(__virtualenv)$RESET"
-
   # path
-  # PS1+="$BG_BASE00$FG_BASE3 $(__getpwd) $RESET"
-  PS1+="$BG_BASE0$FG_BASE3 "'$(__getpwd \w)'" $RESET"
+  PS1+="\[$FG_BASE01\]"
 
-  # git status
-  PS1+="$BG_BASE01$FG_BASE00"'$(__git_info)'"$FG_BASE01"'$BG_EXIT'"$PROMPT_DIVIDER$RESET"
+  PS1+='$(__getpwd \w)'
+
+  # git
+  PS1+="\[$RESET$FG_BASE1$BG_BASE2\]$PROMPT_DIVIDER\[$FG_BASE02\]"
+
+  PS1+='$(__git_info)'
+
+  PS1+="\[$RESET$FG_BASE2"'$BG_EXIT'"\]$PROMPT_DIVIDER"
   # segment transition
-  PS1+='$FG_EXIT'"$PROMPT_DIVIDER$RESET "
+  PS1+='\[$RESET$FG_EXIT\]'
+  PS1+="$PROMPT_DIVIDER\[$RESET\]"
 
   PROMPT_COMMAND=ps1
   PROMPT_DIRTRIM=2
